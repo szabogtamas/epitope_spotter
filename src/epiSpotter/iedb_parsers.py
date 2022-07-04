53a144be1450dfbed407c4a5536094116c1c6ed1
szabogtamas <szabogtamas@gmail.com>
2022-07-04 14:22:20 +0200

    Added parser for MHC table in IEDB

--- src/epiSpotter/iedb_parsers.py
+++ src/epiSpotter/iedb_parsers.py
@@ -3,3 +3,11 @@ import pandas as pd
 def assay_table_parser(fn):
     df = pd.read_csv(fn, skiprows=1)
     return df
+
+def mhc_table_parser(fn):
+    df = pd.read_csv(fn, skiprows=1)
+    col_subset = [
+        "Description", "Parent Protein Accession", "Parent Protein", "Parent Species",
+        "Host ID", "Allele Name", "MHC allele class", "Qualitative Measure"
+    ]
+    return df.loc[:, col_subset]
