eee7adb719e271e834484e42f5b5aa74e98aa9a8
szabogtamas <szabogtamas@gmail.com>
2022-07-07 20:00:24 +0200

    Adding units to mhc assay selection

--- src/epiSpotter/iedb_parsers.py
+++ src/epiSpotter/iedb_parsers.py
@@ -8,6 +8,6 @@ def mhc_table_parser(fn):
     df = pd.read_csv(fn, skiprows=1)
     col_subset = [
         "Description", "Parent Protein Accession", "Parent Protein", "Parent Species",
-        "Host ID", "Allele Name", "MHC allele class", "Qualitative Measure"
+        "Host ID", "Allele Name", "MHC allele class", "Qualitative Measure", "Units"
     ]
     return df.loc[:, col_subset]
